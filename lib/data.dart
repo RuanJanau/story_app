4450ac5c27f6bd383d78b2cc450d8a8c8f3636ae
RhuanJanau <rhuanjanau531@gmail.com>
2022-09-17 12:07:26 -0300

    add data

--- lib/data.dart
+++ lib/data.dart
@@ -1,1 +1,12 @@
-
+List<String> images = [
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.jpg",
+];
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreamind Moon",
+  "Jack the Persian and the black Castel",
+];
